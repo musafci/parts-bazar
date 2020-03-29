d7cd135f9a1f047a9d568d99e1115d754caf99c8
Md Abu Musa <musa.fci@gmail.com>
2020-03-29 18:02:16 +0600

    Initial Commit-Backend All Blade File Ready

---
+++ resources/views/backoffice/app.blade.php
@@ -0,0 +1,22 @@
+﻿<!DOCTYPE html>
+<html>
+
+    @include('backoffice._partial._head')
+
+<body class="theme-deep-purple">
+
+    @include('backoffice._partial._search')
+
+    @include('backoffice._partial._topbar')
+
+    @include('backoffice._partial._sidebar')
+
+    <section class="content">
+        @section('content')
+        @show
+    </section>
+
+    @include('backoffice._partial._script')
+</body>
+
+</html>
